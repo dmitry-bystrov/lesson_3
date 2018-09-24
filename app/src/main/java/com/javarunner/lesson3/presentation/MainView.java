c8ba0a06fc664f434910ec09cad04c6ada1563fc
dmitry_bystrov <dapper.chita@gmail.com>
2018-09-25 00:10:09 +0900

    - добавил OneExecutionStateStrategy для диалога

--- app/src/main/java/com/javarunner/lesson3/presentation/MainView.java
+++ app/src/main/java/com/javarunner/lesson3/presentation/MainView.java
@@ -2,6 +2,7 @@ package com.javarunner.lesson3.presentation;
 
 import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
@@ -12,12 +13,18 @@ import java.io.File;
 public interface MainView extends MvpView {
     @StateStrategyType(SkipStrategy.class)
     void openFileChooser();
+
     @StateStrategyType(SkipStrategy.class)
     void showConvertingSuccessfulMessage();
+
     @StateStrategyType(SkipStrategy.class)
     void showConvertingUnsuccessfulMessage();
 
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showAbortDialog();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void closeAbortDialog();
+
     void showConvertedImage(File imageFile);
 }
